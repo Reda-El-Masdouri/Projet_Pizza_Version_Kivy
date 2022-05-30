f70f4d1cc083fcb28e07f040a4c4e5f010cbd0dd
Reda E <reda.elmasdouri@gmail.com>
2022-05-30 20:52:36 +0200

    style du texte et image de fond

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from kivy.app import App
 from kivy.properties import *
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.behaviors import CoverBehavior
 from kivy.uix.widget import Widget
 
 from models import Pizza
@@ -15,7 +16,7 @@ class PizzaWidget(BoxLayout):
     vegetarienne = BooleanProperty()
 
 
-class MainWidget(BoxLayout):
+class MainWidget(FloatLayout):
     recycleView = ObjectProperty(None)
 
     def __init__(self, **kwargs):
